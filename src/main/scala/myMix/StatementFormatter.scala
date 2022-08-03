e00c329896af9bee06939f5e817f5a1ec5578390
Tom Rafferty <86563641+TomRafferty@users.noreply.github.com>
2022-08-03 14:29:54 +0100

    indexing and formatting fixed - problem date sort
    the previous indexing issue is fixed and now only 1/3 tests fail with the exception being the date sorting functionality.

--- src/main/scala/myMix/StatementFormatter.scala
+++ src/main/scala/myMix/StatementFormatter.scala
@@ -13,30 +13,25 @@ class StatementFormatter{
     }else{
       StatementHeader
     }
-
-  }
-  private def sortTransactions(txns: Seq[Transaction]) =
-    txns.sortBy(tx => tx.getDate)
-
-  //I don't understand the syntax used here.
-  //is it like 2 functions running on a
-  //conditional depending on the params passed?
-  private def formatTransactions: Double => Seq[Transaction] => String = (startingBalance: Double) => {
-    case Nil => ""
-    case tx :: txs =>
-      formatTransactions(startingBalance + tx.getAmount)(txs)
-        .concat(formatLine(tx.getAmount, tx.getDate, startingBalance + tx.getAmount))
   }
 
-  //TODO the count here is indexing wrong -> figure out where it's going wrong
+  //TODO this is not functioning correctly.
+  private def sortTransactions(txns: Seq[Transaction]) = txns.sortBy(tx => tx.getDate)
+
   @tailrec
   private def newFormatter(transactions: Seq[Transaction], thisString: String = "", count: Int = 0, startingBalance: Double = 0): String ={
     val newCount = count + 1
-    val currentTxn = transactions(count)
     if(count == transactions.length){
       thisString
     }else{
-      val newString = s"$thisString\n${currentTxn.getAmount}, ${currentTxn.getDate}, ${currentTxn.getAmount + startingBalance}"
+      val currentTxn = transactions(count)
+      val newString = {
+        if(thisString.length > 0){
+          s"\n$thisString\n${currentTxn.getAmount},${currentTxn.getDate},${currentTxn.getAmount + startingBalance}"
+        }else{
+          s"${currentTxn.getAmount},${currentTxn.getDate},${currentTxn.getAmount + startingBalance}"
+        }
+      }
       newFormatter(transactions = transactions,thisString = newString, count = newCount, startingBalance = startingBalance+currentTxn.getAmount)
     }
   }
