eadb926f9def5bfb37f988cb4b29669cca6c6b12
Tom Rafferty <86563641+TomRafferty@users.noreply.github.com>
2022-08-04 15:55:06 +0100

    got rid of imported reference files
    also fixed balance bug (when no transactions were made, the balance would return a weird value)

--- src/main/scala/myMix/Account.scala
+++ src/main/scala/myMix/Account.scala
@@ -5,8 +5,12 @@ import scala.collection.mutable.ArrayBuffer
 
 class Account {
   var transactions: ArrayBuffer[Transaction] = ArrayBuffer()
-  def balance {
-    transactions.map(txn => txn.getAmount).sum
+  def balance():Double = {
+    if(transactions.length > 0) {
+      transactions.map(txn => txn.getAmount).sum
+    }else{
+      0.0
+    }
   }
 
   def deposit(amount: Double, date: LocalDate): Unit = {
--- src/main/scala/oobank/OOTransaction.scala
+++
@@ -1,8 +0,0 @@
-package oobank
-
-import java.time.LocalDate
-
-class OOTransaction(_amount: Int, _date: LocalDate) {
-  var amount = _amount
-  var date = _date
-}
